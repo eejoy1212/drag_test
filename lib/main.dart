b15760bacffdd5e7bec019deb966181387295e29
eejoy1212 <eejoy1212@gmail.com>
2021-11-09 16:39:40 +0900

    드래그 테스트

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var top = 10.0;
+  var left = 10.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('위젯 드래그 테스트'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: GestureDetector(
+        child: Stack(
+          children: [
+            Positioned(
+                top: top,
+                left: left,
+                child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purpleAccent)),
+                    child: Text('나를 드래그해봐')))
+          ],
+        ),
+        onVerticalDragUpdate: (DragUpdateDetails dd) {
+          print(dd);
+          setState(() {
+            top = dd.localPosition.dy;
+            left = dd.localPosition.dx;
+          });
+        },
+      ),
+    );
+  }
+}
